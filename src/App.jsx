cd0922badb4ee1889f5f97d08aa8aba2b3f62b0f
João Souza <joaofslucas2023@gmail.com>
2024-05-17 19:23:21 +0000

    criação dos primeiros estilos

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./pages/Home"
+
+export default function App() {
+  return (
+    <RouterProvider router = {router} />
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  }
+])
